bc7122d319a1d733618029c59ca13ba872ced42c
anthony-affinda <anthony.malkoun@affinda.com>
2023-05-29 08:42:55 +0000

    #autocommit #update #java - Add required fields for resthook subscriptions

--- affinda-api-client/src/main/java/com/affinda/api/client/models/ResthookSubscription.java
+++ affinda-api-client/src/main/java/com/affinda/api/client/models/ResthookSubscription.java
@@ -9,46 +9,46 @@ public final class ResthookSubscription {
     /*
      * Resthook subscription's ID.
      */
-    @JsonProperty(value = "id")
-    private Integer id;
+    @JsonProperty(value = "id", required = true)
+    private int id;
 
     /*
      * The event name to subscribe to.
      */
-    @JsonProperty(value = "event")
+    @JsonProperty(value = "event", required = true)
     private ResthookEvent event;
 
     /*
      * URL of the resthook's receiver.
      */
-    @JsonProperty(value = "targetUrl")
+    @JsonProperty(value = "targetUrl", required = true)
     private String targetUrl;
 
     /*
      * Resthooks only fire for active subscriptions.
      */
-    @JsonProperty(value = "active")
-    private Boolean active;
+    @JsonProperty(value = "active", required = true)
+    private boolean active;
 
     /*
      * Resthook subscriptions can be auto deactivated if the receiver
      * continuously returns error status code over a period of time.
      */
-    @JsonProperty(value = "autoDeactivated")
-    private Boolean autoDeactivated;
+    @JsonProperty(value = "autoDeactivated", required = true)
+    private boolean autoDeactivated;
 
     /*
      * The reason for the subscription being auto deactivated. May contains the
      * error response that the receiver returned.
      */
-    @JsonProperty(value = "autoDeactivateReason")
+    @JsonProperty(value = "autoDeactivateReason", required = true)
     private String autoDeactivateReason;
 
     /*
      * Version of the resthook subscription. Determines the resthook body being
      * fired.
      */
-    @JsonProperty(value = "version")
+    @JsonProperty(value = "version", required = true)
     private ResthookSubscriptionVersion version;
 
     /**
@@ -56,7 +56,7 @@ public final class ResthookSubscription {
      *
      * @return the id value.
      */
-    public Integer getId() {
+    public int getId() {
         return this.id;
     }
 
@@ -66,7 +66,7 @@ public final class ResthookSubscription {
      * @param id the id value to set.
      * @return the ResthookSubscription object itself.
      */
-    public ResthookSubscription setId(Integer id) {
+    public ResthookSubscription setId(int id) {
         this.id = id;
         return this;
     }
@@ -116,7 +116,7 @@ public final class ResthookSubscription {
      *
      * @return the active value.
      */
-    public Boolean isActive() {
+    public boolean isActive() {
         return this.active;
     }
 
@@ -126,7 +126,7 @@ public final class ResthookSubscription {
      * @param active the active value to set.
      * @return the ResthookSubscription object itself.
      */
-    public ResthookSubscription setActive(Boolean active) {
+    public ResthookSubscription setActive(boolean active) {
         this.active = active;
         return this;
     }
@@ -137,7 +137,7 @@ public final class ResthookSubscription {
      *
      * @return the autoDeactivated value.
      */
-    public Boolean isAutoDeactivated() {
+    public boolean isAutoDeactivated() {
         return this.autoDeactivated;
     }
 
@@ -148,7 +148,7 @@ public final class ResthookSubscription {
      * @param autoDeactivated the autoDeactivated value to set.
      * @return the ResthookSubscription object itself.
      */
-    public ResthookSubscription setAutoDeactivated(Boolean autoDeactivated) {
+    public ResthookSubscription setAutoDeactivated(boolean autoDeactivated) {
         this.autoDeactivated = autoDeactivated;
         return this;
     }
